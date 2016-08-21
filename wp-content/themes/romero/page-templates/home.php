1c5f9612fb6e21b162df543f82b3e6fdccc3cf50
Jay Logan <jlogan@mymolddetective.com>
2016-08-21 18:26:03 -0400

    New home page template.

---
+++ wp-content/themes/romero/page-templates/home.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Full width page template for videos.
+ * Template Name: Home Page
+ *
+ * @package Romero
+ */
+
+	get_header();
+?>
+	<header class="entry-header header-overlap">
+		<div class="container">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</div>
+	</header>
+
+	<div class="container hfeed">
+		<div class="main">
+
+			<div class="main-content full-width">
+<?php
+	if ( have_posts() ) {
+		while ( have_posts() ) {
+			the_post();
+			get_template_part( 'content-page' );
+		}
+	}
+?>
+			</div>
+<?php
+	get_footer();
